57248472c5280481ea759ed2f12acd9aae3215fa
Oleg <lego5621@gmail.com>
2021-07-06 23:49:33 +0600

    добавил роутер поиска

---
+++ src/store/modules/reporting/actions/search.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+export default async function ({ commit, getters, dispatch, state }) {
+    let url =`http://localhost:3000/api/v1/search`
+
+
+    axios.get(url)
+        .then(function (res) {
+            const allCompany =  res.data
+            commit('search', allCompany.company)
+        })
+
+    // const res = await fetch(url)
+    // const allCompany = await res.json()
+
+    // commit('pages', allCompany)
+    // commit('allCompany', allCompany.company)
+}
